8d6e87aa36f4965c44b5ad5ca29a20af211869f1
Hambloko <hambloko@gmail.com>
2023-02-19 00:49:52 -0600

    Initial commit. Will only output a list of available models in JSON.

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use reqwest::header;
+use header::HeaderMap;
+use reqwest::Client;
+struct ClientInterface {
+    key: String,
+    url: String,
+    client: Client,
+    headers: HeaderMap
+}
+
+impl ClientInterface {
+    pub fn new() -> Self {
+        Self {   
+                key: String::from("yourkeyhere"), 
+                url: String::from("https://api.openai.com/v1/models"), 
+                client: Client::new(), 
+                headers: HeaderMap::new()
+        }
+    }
+    pub fn insert_headers(&mut self) {
+        self.headers.insert("Authorization", header::HeaderValue::from_str(&format!("Bearer {}", self.key)).unwrap());
+    }
+    pub async fn get_models(&self) -> Result<String, reqwest::Error> {
+        let response = self.client.get(&self.url)
+            .headers(self.headers.clone())
+            .send()
+            .await?;
+        let output = response.text().await?;
+        Ok(output)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut ci = ClientInterface::new();
+    ci.insert_headers();
+    let models = ci.get_models().await.unwrap();
+
+    println!("{}", models);
+}
